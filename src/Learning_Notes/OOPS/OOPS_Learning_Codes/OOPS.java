ed4f989a881a6d8a06bd30a3938a04adcf15bff6
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-01-09 22:53:00 +0530

    Created 4Months Interview Prep Added Primary files such as LearningCode & Theory Notes

---
+++ src/Learning_Notes/OOPS/OOPS_Learning_Codes/OOPS.java
@@ -0,0 +1,48 @@
+package Learning_Notes.OOPS.OOPS_Learning_Codes;
+
+public class OOPS {
+    public static void main(String[] args) {
+        // Creating object for pen Class
+        Pen reynolds = new Pen();
+        reynolds.color = "red";
+        System.out.println(reynolds.color);
+        reynolds.setColor("Purple");
+        System.out.println(reynolds.color);
+
+        reynolds.tip = 6;
+        System.out.println(reynolds.tip);
+        reynolds.setTip(4);
+        System.out.println(reynolds.tip);
+
+        // Creating object for Bank Class
+        BankAccount sbi = new BankAccount();
+        sbi.username = "Sideonkaves";
+        sbi.setPassword("this is the password");
+        // sbi.password = "this";
+
+    }
+}
+
+//
+class BankAccount {
+    // Access Specifiers
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+        this.password = pwd;
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTip(int tip) {
+        this.tip = tip;
+    }
+}
