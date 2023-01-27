1cdb7ab054024e34693af45761ed24f74f99f8f9
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-01-28 01:04:13 +0530

    Updated Star_Pattern1

--- src/Learning_Notes/Patterns/Star_Pattern1.java
+++ src/Learning_Notes/Patterns/Star_Pattern1.java
@@ -11,11 +11,23 @@ package Learning_Notes.Patterns;
 public class Star_Pattern1 {
   public static void main(String[] args) {
     int rows = 4;
+
+    // ! Upward Half-right triangle
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j <= i; j++) {
-        System.out.println("*");
+        System.out.print("*");
       }
-      
+      System.out.println("");
+    }
+
+    System.out.println("-----------");
+
+    // ! Downward Half-right triangle
+    for (int i = rows; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
     }
 
   }
