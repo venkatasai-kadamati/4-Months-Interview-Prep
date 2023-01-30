733953e478c3dda19be69bc23c9e76056e746d56
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-01-31 02:03:20 +0530

    updated

--- OOPS/Codes/OOPS.java
+++ OOPS/Codes/OOPS.java
@@ -1,4 +1,4 @@
-package Learning_Notes.OOPS.Codes;
+package OOPS.Codes;
 
 public class OOPS {
     public static void main(String[] args) {
--- Stacks/Codes/PushAtBottom.java
+++ Stacks/Codes/PushAtBottom.java
@@ -1,4 +1,4 @@
-package Learning_Notes.Stacks.Codes;
+package Stacks.Codes;
 
 import java.util.*;
 
