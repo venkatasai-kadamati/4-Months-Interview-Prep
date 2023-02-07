e20575771f33e490da6f48fa30847d49a5546a00
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-02-07 14:35:28 +0530

    two sum

---
+++ leetcode_Solutions/1.Two_Sum/BruteForce.java
@@ -0,0 +1,15 @@
+// ! We simply use two loops: double pointer appraoch, traversal technique.
+// ** The key is to return a array of two element representing the indices.
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[] { i, j };
+        }
+      }
+    }
+    return new int[] {};
+  }
+}
---
+++ leetcode_Solutions/1.Two_Sum/Optimized.java
@@ -0,0 +1,7 @@
+// ! We simply optimize it using a hashmap, as the primary task of lookup arises. 
+/* A + B = target
+A = target - B
+So if B is known we need to simply lookup for A */
+public class Optimized {
+
+}
